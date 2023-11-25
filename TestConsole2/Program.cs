da3afd084b973bef2b3c4bc6de573e7a0aff3e08
ptyankkaaa <ptyankkaaa@yandex.ru>
2023-11-26 04:43:23 +0500

    п

--- TestConsole2/Program.cs
+++ TestConsole2/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsole;
+
+class Program
+{
+    public static double Calculate(double amountOFcredit, double interestRate, double depositTerm)
+    {
+        return amountOFcredit * Math.Pow((1+(interestRate/(100*12))),depositTerm);
+    }
+    static void Main(string[] args)
+    {
+    
+        Console.WriteLine("Enter data:");
+        double[] str = Console.ReadLine().Split().Select(double.Parse).ToArray();
+        Console.WriteLine($"SUM = {Calculate(str[0], str[1], str[2])}");
+    }
+}
