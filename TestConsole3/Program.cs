149604a897f501fe5ce4ec6c4018033e3dcea5c6
ptyankkaaa <ptyankkaaa@yandex.ru>
2023-11-27 23:31:46 +0500

    /

---
+++ TestConsole3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsole;
+
+class Program
+{
+    public static double Calculate(string userInput)
+    {
+        string[] values = userInput.Split(' ');
+        var amountOFcredit = Convert.ToDouble(values[0]);
+        var interestRate = Convert.ToInt32(values[1]);
+        var depositTerm = Convert.ToInt32(values[2]);
+        return amountOFcredit * Math.Pow((1+(interestRate/(100*12))),depositTerm);
+    }
+    static void Main(string[] args)
+    {
+    
+        Console.WriteLine("Enter data:");
+        double[] str = Console.ReadLine();
+        Console.WriteLine($"SUM = {Calculate(str)}");
+    }
+}
